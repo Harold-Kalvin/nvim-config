c4fce5202462206fb46a5da922d1888a548f432c
harold <harold.k.basa@gmail.com>
2025-04-19 18:37:45 +0200

    highlight yank

--- lua/kalzone/core/init.lua
+++ lua/kalzone/core/init.lua
@@ -1,3 +1,11 @@
 require("kalzone.core.options")
 require("kalzone.core.keymaps")
 
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
