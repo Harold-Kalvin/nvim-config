2131c3ba7f419ac84d1fdb5a59f5d8b05a166c92
harold <harold.k.basa@gmail.com>
2025-04-19 17:57:26 +0200

    replace telescope and nvim-tree by snacks equivalent

--- lua/kalzone/plugins/snacks.lua
+++ lua/kalzone/plugins/snacks.lua
@@ -4,11 +4,34 @@ return {
   lazy = false,
   opts = {
     indent = {
-      enabled = true,
       animate = {
         enabled = false,
       },
     },
-    input = { enabled = true },
+    notifier = {},
+    input = {},
+    picker = {
+      sources = {
+        explorer = {
+          layout = { layout = { position = "right" } },
+        }
+      }
+    },
+    explorer = {},
   },
+  keys = {
+    -- picker
+    { "<leader>ff", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+    { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+    { "<leader>fn", function() Snacks.picker.notifications() end, desc = "Notification History" },
+    { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
+    -- explorer
+    { "<leader>ee", function() Snacks.explorer() end, desc = "File Explorer" },
+    -- LSP
+    { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
+    { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
+    { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+    { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
+    { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+  }
 }
