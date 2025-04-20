f81058f4658d7f486f6d7c0e061c5cc5bd4d61a8
harold <harold.k.basa@gmail.com>
2025-04-20 13:04:36 +0200

    only show buffers in statusline

--- lua/kalzone/plugins/lualine.lua
+++ lua/kalzone/plugins/lualine.lua
@@ -10,7 +10,12 @@ return {
         section_separators = "",
       },
       sections = {
-        lualine_a = {"buffers"}
+        lualine_a = { "buffers" },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
       },
     })
   end,
