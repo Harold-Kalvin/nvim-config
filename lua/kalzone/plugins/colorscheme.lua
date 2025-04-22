a90153811b1476401ca3e88ba83587b9c8a27f8c
harold <harold.k.basa@gmail.com>
2025-04-22 11:59:21 +0200

    update comment

--- lua/kalzone/plugins/colorscheme.lua
+++ lua/kalzone/plugins/colorscheme.lua
@@ -5,7 +5,7 @@ return {
   config = function()
     require("catppuccin").setup({
       flavour = "mocha",
-      transparent_background = true, -- set terminal bg color to #1e1e2e
+      transparent_background = true, -- on terminal, use catppunccin theme or set bg color to #1e1e2e
       blink_cmp = true,
     })
     vim.cmd.colorscheme("catppuccin")
